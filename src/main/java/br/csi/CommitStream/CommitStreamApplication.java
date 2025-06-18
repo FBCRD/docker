9d911eea31f42ccda837fe0b97f13b1b6b1441eb
FBCRD <142154318+FBCRD@users.noreply.github.com>
2025-06-18 19:50:22 -0300

    update

--- src/main/java/br/csi/CommitStream/CommitStreamApplication.java
+++ src/main/java/br/csi/CommitStream/CommitStreamApplication.java
@@ -13,6 +13,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -29,8 +31,12 @@ import java.util.Set;
 		)
 )
 @SpringBootApplication
+@RestController
 public class CommitStreamApplication {
-
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello from Dockerized Spring Boot!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CommitStreamApplication.class, args);
 	}
